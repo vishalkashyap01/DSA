e0cb3142c26afd9226c0e66c21ad580a05c6b091
Vishal Kashyap <147410216+vishalkashyap01@users.noreply.github.com>
2024-07-26 07:36:18 +0530

    Add MaxMinElement class
    add MaxMinElement Class to search the max & min element from the array. create object to call into the main method.

--- Array.java
+++ Array.java
@@ -3,12 +3,62 @@ import java.util.Random;
 public class Array {
     public static void main(String []ds) {
 
-        Reverse rev = new Reverse();
+        //for reverse classs object & methods
+        /*Reverse rev = new Reverse();
         rev.printArray();
         rev.reverseArray();
+        */
         
+        //max. & min. element in array
+        MaxMinElement mm = new MaxMinElement();
+        int max = mm.getMax();
+        int min = mm.getMin();
+        System.out.println("Maximum element is  " + max);
+        System.out.println("Minimun value is  " + min);
+        mm.printArray();
+
     }
 }
+
+class MaxMinElement {
+    Random random = new Random();
+    int []element = new int[5];
+    private int max, min;
+
+
+    MaxMinElement() {
+        for(int count=0; count<5; count++)
+        {
+            element[count] = random.nextInt(10)+1;
+        }
+        //max = element[0];
+        //min = element[0];
+        for (int i=0; i< element.length-1; i++) {
+            for (int j=0; j< element.length-i-1; j++) {
+                if (element[j] > element[j+1]) {
+                    int temp = element[j];
+                    element[j] = element[j+1];
+                    element[j+1] = temp;
+                }
+            }
+        }
+    }
+    void printArray() {
+        for (int counter=0; counter<element.length; counter++)
+        {
+            System.out.print("  " + element[counter]);
+        }
+    }
+    int getMax() {
+       max = element[element.length-1];
+       return max;
+    }
+    int getMin() {
+        min = element[0];
+        return min;
+    }
+}
+
 
 class Reverse {
     Random random = new Random();
