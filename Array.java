8d7a211ebbda20324a2996de0d77a5f555da145d
Vishal Kashyap <147410216+vishalkashyap01@users.noreply.github.com>
2024-08-03 18:31:35 +0530

    Update Array.java
    make a class named kthElement by which its methods we can search the K^th Maximum & Minimum vlaue of the array elements.

--- Array.java
+++ Array.java
@@ -1,7 +1,9 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Array {
     public static void main(String []ds) {
+        Scanner sc = new Scanner(System.in);
 
         //for reverse classs object & methods
         /*Reverse rev = new Reverse();
@@ -9,16 +11,56 @@ public class Array {
         rev.reverseArray();
         */
         
-        //max. & min. element in array
+        /*max. & min. element in array
         MaxMinElement mm = new MaxMinElement();
         int max = mm.getMax();
         int min = mm.getMin();
         System.out.println("Maximum element is  " + max);
         System.out.println("Minimun value is  " + min);
         mm.printArray();
+        */
+
+        kthElement kmp = new kthElement();
+        int kthMax, kthMin;
+        System.out.print("Put maximum kth : ");     //2
+        int m = sc.nextInt();
+        System.out.print("Put minimum kth : ");     //3
+        int n = sc.nextInt();
+        kthMax = kmp.getKthMax(m);
+        kthMin = kmp.getKthMin(n);
+        System.out.println("Max. value is " + kthMax);
+        System.out.println("Max. value is " + kthMin);
 
     }
 }
+
+
+class kthElement {
+        //Scanner sc = new Scanner(System.in);
+        private int arr[] = {4, 3, 2, 1, 15, 12, 10};
+        private int kthMax, kthMin = 0;
+
+        kthElement() {
+            for (int i=0; i< arr.length-1; i++) {
+                for (int j=0; j< arr.length-i-1; j++) {
+                    if (arr[j] > arr[j+1]) {
+                        int temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+                }
+            }
+        }
+        int getKthMax(int kMax) {
+              kthMax = arr[arr.length-1];   //subtracting (1) from array length bcz maintain zero indexing to get exact value.
+              return kthMax;
+        }
+        int getKthMin(int kMin) {
+           kthMin = arr[kMin-1];
+           return kthMin;
+        }
+}
+
 
 class MaxMinElement {
     Random random = new Random();
